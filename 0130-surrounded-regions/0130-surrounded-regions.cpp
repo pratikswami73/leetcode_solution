f384f11ac15799bdf8d294213e3f8beba750424b
PRATIK SWAMI <pratikswami85@gmail.com>
2026-09-09 22:13:12 +0530

    Time: 5 ms (7.73%), Space: 17 MB (8.29%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,104 @@
+class Solution {
+public:
+
+    queue<pair<int,int>> q;
+    queue<pair<int,int>> q2;
+
+    void call(vector<vector<char>>& board,
+              vector<vector<bool>>& vis,
+              int i, int j) {
+
+        bool boundary = false;
+
+        vis[i][j] = true;
+        q.push({i, j});
+
+        while (!q.empty()) {
+
+            auto it = q.front();
+            q.pop();
+
+            int x = it.first;
+            int y = it.second;
+
+            q2.push({x, y});
+
+            if (x == 0 || y == 0 ||
+                x == board.size() - 1 ||
+                y == board[0].size() - 1) {
+
+                boundary = true;
+            }
+
+            if (x - 1 >= 0 &&
+                !vis[x - 1][y] &&
+                board[x - 1][y] == 'O') {
+
+                vis[x - 1][y] = true;
+                q.push({x - 1, y});
+            }
+
+            if (y - 1 >= 0 &&
+                !vis[x][y - 1] &&
+                board[x][y - 1] == 'O') {
+
+                vis[x][y - 1] = true;
+                q.push({x, y - 1});
+            }
+
+            if (x + 1 < board.size() &&
+                !vis[x + 1][y] &&
+                board[x + 1][y] == 'O') {
+
+                vis[x + 1][y] = true;
+                q.push({x + 1, y});
+            }
+
+            if (y + 1 < board[0].size() &&
+                !vis[x][y + 1] &&
+                board[x][y + 1] == 'O') {
+
+                vis[x][y + 1] = true;
+                q.push({x, y + 1});
+            }
+        }
+
+        if (!boundary) {
+
+            while (!q2.empty()) {
+
+                auto it = q2.front();
+                q2.pop();
+
+                board[it.first][it.second] = 'X';
+            }
+        }
+        else {
+
+            while (!q2.empty()) {
+                q2.pop();
+            }
+        }
+    }
+
+    void solve(vector<vector<char>>& board) {
+
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<bool>> vis(
+            n, vector<bool>(m, false)
+        );
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+
+                if (board[i][j] == 'O' && !vis[i][j]) {
+
+                    call(board, vis, i, j);
+                }
+            }
+        }
+    }
+};
